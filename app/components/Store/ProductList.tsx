ec9bb842ff9638317c9e0adb0ee98779a4d3cb19
Andrei Gatej <andreigtj01@gmail.com>
2023-10-11 15:02:46 +0300

    feat(Store/Products): show 'Add to cart' on card hover

--- app/components/Store/ProductList.tsx
+++ app/components/Store/ProductList.tsx
@@ -15,10 +15,12 @@ function ProductList(props: Props) {
         products.map(p => (
           <li className="" key={p.name}>
             {/* Could extract if further in a `ProductCard` component. */}
-            <div>
-              <div className="w-[282px] h-[450px]">
+            <div className="group">
+              <div className="relative w-[282px] h-[450px]">
                 <img className="h-full w-full" src={getImageSrc(p)} alt="" />
+                <button className="hidden group-hover:block absolute uppercase bottom-0 left-0 right-0 bg-[#000] text-[#fff] pt-1 pb-1">Add to cart</button>
               </div>
+              
               <div>
                 <div className="font-bold text-gray-dark-1 capitalize">{p.category}</div>
                 <div className="font-bold text-xl">{p.name}</div>
